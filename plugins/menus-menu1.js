ff92e844e76a7d42ec2e66efbcbe1f33ee7fbfed
Omar Ashraf <124444958+Omar0837373@users.noreply.github.com>
2024-05-28 16:29:13 +0300

    Update menus-menu1.js

--- plugins/menus-menu1.js
+++ plugins/menus-menu1.js
@@ -116,7 +116,7 @@ export default handler
     conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
   }
 };
-handler.command = /^(menu1)$/i;
+handler.command = /^(menu)$/i;
 handler.exp = 50;
 handler.fail = null;
 export default handler;
