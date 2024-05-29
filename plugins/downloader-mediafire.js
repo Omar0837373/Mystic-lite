275af588146f45f5428412a76f0f03db54387bc4
Omar Ashraf <124444958+Omar0837373@users.noreply.github.com>
2024-05-29 16:57:14 +0300

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -4,15 +4,15 @@ import cheerio from 'cheerio';
 import {mediafiredl} from '@bochilteam/scraper';
 
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-  if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*`;
+  if (!args[0]) throw `*[❗خطأ❗] أدخل رابط صالح مثل: ${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*`;
   try {
     const resEX = await mediafiredl(args[0]);
     const captionES = `
-*📓 𝙽𝙾𝙼𝙱𝚁𝙴:* ${resEX.filename}
-*📁 𝙿𝙴𝚂𝙾:* ${resEX.filesizeH}
-*📄 𝚃𝙸𝙿𝙾:* ${resEX.ext}
+*📓 الأسم:* ${resEX.filename}
+*📁 الحجم:* ${resEX.filesizeH}
+*📄 النوع:* ${resEX.ext}
 
-*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
+*⏳ أنتظر يتم تجهيز الملف. . . .* 
 `.trim();
     m.reply(captionES);
     await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, {mimetype: resEX.ext, asDocument: true});
@@ -21,22 +21,22 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
       const res = await mediafireDl(args[0]);
       const {name, size, date, mime, link} = res;
       const caption = `
-*📓 𝙽𝙾𝙼𝙱𝚁𝙴:* ${name}
-*📁 𝙿𝙴𝚂𝙾:* ${size}
-*📄 𝚃𝙸𝙿𝙾:* ${mime}
+*📓 الأسم:* ${name}
+*📁 الحجم:* ${size}
+*📄 النوع:* ${mime}
 
-*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
+*⏳ أنتظر يتم تجهيز الملف. . . .* 
 `.trim();
       await m.reply(caption);
       await conn.sendFile(m.chat, link, name, '', m, null, {mimetype: mime, asDocument: true});
     } catch {
-      await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*');
+      await m.reply('*[❗خطأ❗] يرجي المحاولة مرة أخري*\n\n*- مثال عن رسالة الخطأ:*\n*◉ https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*\n*عمر بيمسي*');
     }
   }
 };
 handler.help = ['mediafire'].map((v) => v + ' <url>');
 handler.tags = ['downloader'];
-handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i;
+handler.command = /^(mediafire|mediafiredl|dlmediafire|مديا فاير)$/i;
 export default handler;
 
 async function mediafireDl(url) {
